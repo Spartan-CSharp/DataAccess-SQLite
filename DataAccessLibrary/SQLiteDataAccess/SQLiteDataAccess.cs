6fb9fc8003aa2c1c14aa87740ac94129839ef0d2
Pierre Plourde <pierre@spartancsharp.net>
2024-02-13 12:54:52 -0500

    Code Cleanup
    Clear Errors, Warnings, and Messages; run Code Cleanup Profile

--- DataAccessLibrary/SQLiteDataAccess/SQLiteDataAccess.cs
+++ DataAccessLibrary/SQLiteDataAccess/SQLiteDataAccess.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
 using System.Linq;
-using System.Text;
 
 using Dapper;
 
